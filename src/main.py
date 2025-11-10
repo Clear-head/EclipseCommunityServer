cfc1ed99afb48d1a1d58b84ed516a58895f1219b
Clear-head <huni032285@gmail.com>
2025-11-10 17:21:28 +0900

    [feat] CORS 추가

--- src/main.py
+++ src/main.py
@@ -1,8 +1,20 @@
 import uvicorn
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+from src.controller import community_controller
 
 app = FastAPI()
-
+app.include_router(community_controller.router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "*"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8081)
+    uvicorn.run(app, host="0.0.0.0", port=8082)
